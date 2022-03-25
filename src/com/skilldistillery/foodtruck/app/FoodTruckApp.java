91a49de4fe33fb232361db21600a0a38fd54833a
Henrymelendez <henryomarm@gmail.com>
2022-03-25 18:53:51 -0400

    push my starter code

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,9 +1,12 @@
 package com.skilldistillery.foodtruck.app;
+import com.skilldistillery.foodtruck.entities.FoodTruck;  // import Truck class from package
 
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		//TODO an array of Food Truck objects;
+		
+		FoodTruck[] ft = new FoodTruck[5];
 		
 		
 
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -2,5 +2,57 @@ package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
 	private static int nextTruckId;
+	private final int MAX_TRUCKS = 5;
+	private String name;
+	private String foodType;
+	private double rating;
+	
+	
+	
+	public FoodTruck(String name, String foodType, double rating) {
+		this.nextTruckId = ++nextTruckId;
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+
+
+	public double getRating() {
+		return rating;
+	}
+
+
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	
 
 }
