d70dfca245f757fb3768860c1b3c20e408ba6b20
Henrymelendez <henryomarm@gmail.com>
2022-03-25 18:59:53 -0400

    added some main logic

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,12 +1,22 @@
 package com.skilldistillery.foodtruck.app;
+import java.util.Scanner;
+
 import com.skilldistillery.foodtruck.entities.FoodTruck;  // import Truck class from package
 
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		//TODO an array of Food Truck objects;
-		
+		Scanner sc = new Scanner(System.in);
 		FoodTruck[] ft = new FoodTruck[5];
+		
+		System.out.println("Please Input a Food truck");
+		System.out.print("Add Food Truck name: ");
+		String name = sc.nextLine();
+		System.out.print("Add the Food Type of the truck: ");
+		String foodType = sc.nextLine();
+		System.out.print("Add a rating: ");
+		sc.nextDouble();
 		
 		
 
